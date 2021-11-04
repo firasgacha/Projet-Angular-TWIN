918d53927144985b7af78f9d1928bc463a848813
Firas GACHA <firas.gacha@esprit.tn>
2021-11-04 09:57:34 +0100

    Main-product product

---
+++ src/app/model/product.ts
@@ -0,0 +1,10 @@
+export class Product{
+  id : string;
+  title : string;
+  price : number;
+  desciption : string;
+  category : string;
+  nbrLike : number;
+  quantity : number;
+  picture : string;
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Product} from "../model/product";
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  //dans le composant fils en met @Input
+  //on va recevoir des doneés
+  @Input() product : Product;
+  //exporter des fonctions vers une autre component
+  @Output() notification : EventEmitter<Product>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  notifyParent(){
+    this.notification = new EventEmitter<Product>();
+    this.notification.emit(this.product);
+  }
 }
